ef8f66ef4ae2953665918b97c1d366c0a8eded9e
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-01-06 01:07:12 -0500

    thumb nails title and views added

--- front-end/src/components/Recommended.js
+++ front-end/src/components/Recommended.js
@@ -14,11 +14,21 @@ const RecommendedList = ({ fetchVideos, dispatch }) => {
   return (
     <div className="w-[88%] display: flex flex-wrap ml-20">
       {fetchVideos.map((videos) => (
-        <div className="w-[24%] h-56 border-2 border-gray-500 my-4 m-[.5%]">
+        <div key={videos.id} className="w-[24%] h-56 my-4 m-[.5%] my-20">
           <img
             className="w-full h-full"
             src={videos.snippet.thumbnails.high.url}
           />
+          <div className="w-[100%] h-[40%] py-[2.5%] flex">
+            <div className="w-10 h-9 border-2 border-gray-500 rounded-full"></div>
+            <div className="w-full h-8 ml-2">
+              <h1 className="text-lg font-semibold">{videos.snippet.title}</h1>
+              <p className="text-sm leading-5">{videos.snippet.channelTitle}</p>
+              <p className="leading-5 text-sm">
+                {videos.statistics.viewCount} views
+              </p>
+            </div>
+          </div>
         </div>
       ))}
     </div>
