8edca8f8c9450cc6e0631b06d50193ca764f7c43
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-07-14 23:40:56 -0400

    changed title and icon

--- front-end/src/components/Recommended.js
+++ front-end/src/components/Recommended.js
@@ -1,16 +1,17 @@
-import React, { useEffect } from "react";
-import { fetchChannels } from "../actions";
-import { connect } from "react-redux";
-import { CHANNELS, API_KEY, URL } from "../api/api";
-import axios from "axios";
-import numeral from "numeral";
+import React, { useEffect } from 'react';
+import { fetchChannels } from '../actions';
+import { connect } from 'react-redux';
+import { CHANNELS, URL } from '../api/api';
+import axios from 'axios';
+import numeral from 'numeral';
 
-const Recommended = ({ video, dispatch, channels }) => {
+const Recommended = ({ video, videos, dispatch, channels }) => {
   const getChannels = () => {
     return (dispatch) => {
       axios
         .get(
-          `${URL}${CHANNELS}${video.snippet.channelId}${process.env.REACT_API_KEY}`
+          `${URL}${CHANNELS}${video.snippet.channelId}${process.env.REACT_APP_API_KEY}`,
+          { params: { part: 'snippet', id: video.snippet.channelId } }
         )
         .then((res) => {
           dispatch(fetchChannels(res.data.items));
@@ -26,29 +27,29 @@ const Recommended = ({ video, dispatch, channels }) => {
   }, []);
 
   let views = video.statistics.viewCount;
-
+  // console.log(channels);
   return (
-    <div className="sm:w-[32.2%] md:w-[90%] md4:w-[32%] h-80 md2:w-[46%] h-72 lg:w-[21%] h-56 m-[.5%]">
+    <div className='sm:w-[32.2%] md:w-[90%] md4:w-[32%] h-80 md2:w-[46%] h-72 lg:w-[21%] h-56 m-[.5%]'>
       <img
-        className="w-full h-full "
+        className='w-full h-full '
         src={video.snippet.thumbnails.high.url}
-        alt=""
+        alt=''
       />
-      <div className="w-[100%] h-[40%] py-[2.5%] flex">
-        <div className="w-9 h-8 border-black border 2 rounded-full">
+      <div className='w-[100%] h-[40%] py-[2.5%] flex'>
+        <div className='w-9 h-8 border-black border 2 rounded-full'>
           <img
-            className="w-full h-full rounded-full"
+            className='w-full h-full rounded-full'
             src={video.snippet.thumbnails.high.url}
-            alt=""
+            alt=''
           />
         </div>
-        <div className="w-full h-8 ml-2">
-          <h1 className="text-lg font-semibold line-clamp-2 ...">
+        <div className='w-full h-8 ml-2'>
+          <h1 className='text-lg font-semibold line-clamp-2 ...'>
             {video.snippet.title}
           </h1>
-          <p className="text-md leading-5">{video.snippet.channelTitle}</p>
-          <p className="leading-6 text-md">
-            {numeral(views).format("a")} views
+          <p className='text-md leading-5'>{video.snippet.channelTitle}</p>
+          <p className='leading-6 text-md'>
+            {numeral(views).format('a')} views
           </p>
         </div>
       </div>
@@ -60,6 +61,7 @@ const mapStateToProps = (state) => {
   return {
     loading: state.loading,
     channels: state.channels,
+    videos: state.videos,
   };
 };
 export default connect(mapStateToProps)(Recommended);
--- front-end/src/components/RecommendedList.js
+++ front-end/src/components/RecommendedList.js
@@ -1,17 +1,40 @@
-import React, { useEffect } from "react";
-import { getVideos } from "../actions";
-import Recommended from "./Recommended";
-import { connect } from "react-redux";
+import React, { useEffect } from 'react';
+import { getVideos } from '../actions';
+import Recommended from './Recommended';
+import { fetchChannels } from '../actions';
+import { CHANNELS, URL } from '../api/api';
+import axios from 'axios';
+import { connect } from 'react-redux';
 
-const RecommendedList = ({ dispatch, videos, loading }) => {
+const RecommendedList = ({ dispatch, videos, channels }) => {
+  const getChannels = () => {
+    return (dispatch) => {
+      axios
+        .get(
+          `${URL}${CHANNELS}${videos[0]?.videos[1]?.snippet.channelId}${process.env.REACT_APP_API_KEY}`,
+          { params: { part: 'snippet', id: videos[0]?.snippet.channelId } }
+        )
+        .then((res) => {
+          dispatch(fetchChannels(res.data.items));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+  };
+
   useEffect(() => {
     dispatch(getVideos());
   }, []);
 
+  useEffect(() => {
+    dispatch(getChannels());
+  }, [getVideos]);
+
   return (
-    <div className="w-[87%] h-2 display: flex flex-wrap justify-center my-32 sm:ml-[6%] md:ml-[12%] md3:ml-[8%] md4:ml-[8%] lg:ml-[13%] gap-y-32">
+    <div className='w-[87%] h-2 display: flex flex-wrap justify-center my-32 sm:ml-[6%] md:ml-[12%] md3:ml-[8%] md4:ml-[8%] lg:ml-[13%] gap-y-32'>
       {videos.map((video) => (
-        <Recommended key={video.id} video={video} />
+        <Recommended key={video.id} video={video} channels={channels} />
       ))}
     </div>
   );
@@ -21,6 +44,7 @@ const mapStateToProps = (state) => {
   return {
     loading: state.loading,
     videos: state.videos,
+    channels: state.channels,
   };
 };
 
